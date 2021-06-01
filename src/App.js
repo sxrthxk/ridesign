e50fc4766d3aa0b42b2df60a6938135a82666db8
Sarthak Gupta <sarthak.ag2001@gmail.com>
2021-06-01 20:43:17 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import './App.css'
 import WorkSamples from "./components/WorkSamples";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (<>
     <Navbar/>
-
     <WorkSamples/>
-
     {/* <CustomCarousel/> */}
-   
-  </>);
+   </>
+  )
 }
 
 export default App;
