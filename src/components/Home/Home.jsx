a5e57dc3424ab4700ac9ed5bf38d8d154a8bab83
sxrthxk <sarthak.ag2001@gmail.com>
2021-06-01 20:50:41 +0530

    hr

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { StyledHR } from '../StyledHR'
 import WorkSamples from '../WorkSamples'
 
 const Home = () => {
     return (
         <>
         <WorkSamples/>
+        <StyledHR/>
         </>
     )
 }
