e6ee0e65d9a6388571dd2b32d012c974aa11445b
sxrthxk <sarthak.ag2001@gmail.com>
2021-06-04 14:47:56 +0530

    updated navbar, added router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import './App.css'
+import "./App.css";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import Services from "./components/Services";
+import WhoWeAre from "./components/WhoWeAre";
+import ContactUs from "./components/ContactUs";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
-  return (<>
-    <Navbar/>
-
-    <Home/>
-    
-    <Footer/>
-  </>);
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/" children={Home} exact />
+        <Route path="/ridesign" children={Home} exact />
+        <Route path="/services" children={Services} />
+        <Route path="/who-we-are" children={WhoWeAre} />
+        <Route path="/contact-us" children={ContactUs} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
-
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,20 +4,22 @@ import { ReactComponent as HamburgerMenuLight } from "../../assets/svg/menuLight
 import { ReactComponent as Cross } from "../../assets/svg/cross.svg";
 import { ReactComponent as BrandLogoDark } from "../../assets/svg/logo-dark.svg";
 import reactDom from "react-dom";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Navbar = () => {
-  const [isAccordianOpen, setisAccordianOpen] = useState(false);
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
   var Logo;
-  isAccordianOpen ? (Logo = Cross) : (Logo = HamburgerMenuLight);
+  isAccordionOpen ? (Logo = Cross) : (Logo = HamburgerMenuLight);
 
 
 
   const toggleAccordion = () => {
-    setisAccordianOpen((v) => !v);
+    setIsAccordionOpen((v) => !v);
   };
 
   const accordionHeight =
-    "navbar-wrapper" + (isAccordianOpen ? " accordOpen" : "");
+    "navbar-wrapper" + (isAccordionOpen ? " accordOpen" : "");
   return (
     reactDom.createPortal(
       <div className="nav-container">
@@ -27,16 +29,24 @@ const Navbar = () => {
         <h1 className="brandname">ridesignMedia</h1>
         <Logo className="hamburg" onClick={toggleAccordion} />
       </div>
-      {isAccordianOpen ? (
+      {isAccordionOpen ? (
         <div className="links">
-          <h1>home</h1>
-          <h1>services</h1>
-          <h1>who we are</h1>
-          <h1>contact us</h1>
+          <StyledLink onClick={toggleAccordion} to="/">home</StyledLink>
+          <StyledLink onClick={toggleAccordion} to="/services">services</StyledLink>
+          <StyledLink onClick={toggleAccordion} to="/who-we-are">who we are</StyledLink>
+          <StyledLink onClick={toggleAccordion} to="contact-us">contact us</StyledLink>
         </div>
       ) : null}
     </div></div>, document.getElementById("header"))
   );
 };
+
+const StyledLink = styled(Link)`
+color: black;
+  font-size: x-large;
+  font-weight: bold;
+  margin: 10px auto;
+  text-decoration: none;
+  `;
 
 export default Navbar;
