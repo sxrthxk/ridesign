506f0d2b7942c314418611c32a396f6de39953a1
Sarthak Gupta <sarthak.ag2001@gmail.com>
2021-08-04 17:15:13 +0530

    test

--- src/assets/models/TestimonialsModel.jsx
+++ src/assets/models/TestimonialsModel.jsx
@@ -1,48 +1,66 @@
-import img1 from '../images/worksamples/1 (2)c.jpg'
 import nuggetsbygrant from '../../assets/images/testimonials/nuggetsbygrant.jpg'
 import paul from '../../assets/images/testimonials/paul.jpg'
+import josh from '../../assets/images/testimonials/josh.jpg'
+import traininsane from '../../assets/images/testimonials/traininsane.jpg'
+import armas from '../../assets/images/testimonials/armas.jpg'
+import itsalbie from '../../assets/images/testimonials/itsalbie.jpg'
+import maximo from '../../assets/images/testimonials/maximo.jpg'
 
 export const TestimonialsModel = [
     {
+        key: 1,
         name: "Grant Connor",
-        designation: "CEO, nuggetsbygrant",
+        designation: "CEO, Gold Digging Agency",
         avi: nuggetsbygrant,
         quote: "The overall service was better than I expected. The team has really helped me to understand the underlying issue and solve it.",
         gradient: "radial-gradient( circle farthest-corner at 10% 20%,  rgba(255,209,67,1) 0%, rgba(255,145,83,1) 90% );"
     },
     {
-        name: "Paul",
-        designation: "CEO, paul",
+        key: 2,
+        name: "Paul Getter",
+        designation: "USA Maketing Consultant",
         avi: paul,
-        quote: "Very Well Work, the work was completed on time and was polished as well. Would definiately like to work again.",
+        quote: "Ridesign is great, their customer service is superb, the guys there respond very quickly and go above and beyond to help.",
         gradient: "radial-gradient( circle farthest-corner at 10% 20%,  rgba(14,174,87,1) 0%, rgba(12,116,117,1) 90% );"
     },
     {
-        name: "John Doe",
-        designation: "CEO",
-        avi: img1,
-        quote: "Very Well Work, the work was completed on time and was polished as well. Would definiately like to work again.",
+        key: 3,
+        name: "Josh Jetson",
+        designation: "CEO, Hendrix Jets",
+        avi: josh,
+        quote: "They helped me in my social media. Their incredibly amazing team helped me in making my dream business a reality.",
         gradient: "radial-gradient( circle farthest-corner at 10% 20%,  rgba(255,94,247,1) 17.8%, rgba(2,245,255,1) 100.2% );"
     },
     {
-        name: "John Doe",
-        designation: "CEO",
-        avi: img1,
-        quote: "Very Well Work, the work was completed on time and was polished as well. Would definiately like to work again.",
+        key: 4,
+        name: "Train Insane Era",
+        designation: "India's Biggest Fitness Trainer",
+        avi: traininsane,
+        quote: "Ridesignmedia helped in my social media management. Their team's understanding of the online business scenario was amazing and helped me in acheiving my goals.",
         gradient: "radial-gradient( circle farthest-corner at 10% 20%,  rgba(97,186,255,1) 0%, rgba(166,239,253,1) 90.1% );"
     },
     {
-        name: "John Doe",
-        designation: "CEO",
-        avi: img1,
-        quote: "Very Well Work, the work was completed on time and was polished as well. Would definiately like to work again.",
+        key: 5,
+        name: "Julian Armas",
+        designation: "Real Estate, NYC",
+        avi: armas,
+        quote: "Absolutely Loved working with ridesign. They captured my vision and accomplished so much in our time together",
         gradient: "linear-gradient( 358.4deg,  rgba(249,151,119,1) -2.1%, rgba(98,58,162,1) 90% );"
     },
     {
-        name: "John Doe",
-        designation: "CEO",
-        avi: img1,
+        key: 6,
+        name: "Albert Albie Aldahawi",
+        designation: "Pro Wrestler",
+        avi: itsalbie,
         quote: "Very Well Work, the work was completed on time and was polished as well. Would definiately like to work again.",
         gradient: "linear-gradient( 134.6deg,  rgba(201,37,107,1) 15.4%, rgba(116,16,124,1) 74.7% );"
+    },
+    {
+        key: 7,
+        name: "Maximo Garcia",
+        designation: "Entrepreneur, Model",
+        avi: maximo,
+        quote: "Our team made a great decision to work with ridesignmedia when we were in need of new branding of our product.",
+        gradient: "linear-gradient( 89.4deg,  rgba(194,0,39,1) 0.8%, rgba(10,35,104,1) 99.4% );"
     },
 ]
