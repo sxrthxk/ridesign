9ad7f4bfba474bbe636b760f7f42e798d46937d2
Sarthak Gupta <sarthak.ag2001@gmail.com>
2021-08-06 12:37:44 +0530

    added images and changed some

--- src/components/Home/WorkSamples.jsx
+++ src/components/Home/WorkSamples.jsx
@@ -2,18 +2,29 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import image1 from "../../assets/images/worksamples/1 (2)c_compressed.jpg";
 import image2 from "../../assets/images/worksamples/2 (2)c_compressed.jpg";
-import image7 from '../../assets/images/worksamples/4 (2)c_compressed.jpg'
-import image8 from '../../assets/images/worksamples/Copy of Untitled (4)c_compressed.jpg'
+import image3 from '../../assets/images/worksamples/4 (2)c_compressed.jpg'
+import image4 from '../../assets/images/worksamples/Copy of Untitled (4)c_compressed.jpg'
+import image5 from '../../assets/images/worksamples/photo_2021-05-25_18-12-46_compressed.jpg'
+import image6 from '../../assets/images/worksamples/photo_2021-05-25_18-14-47_compressed.jpg'
+import image7 from '../../assets/images/worksamples/photo_2021-05-25_18-14-53_compressed.jpg'
+import image8 from '../../assets/images/worksamples/photo_2021-08-06_12-31-17_compressed.jpg'
 import { StyledContainer } from "../UI";
 import Aos from "aos";
 
 const WorkSamples = () => {
   useEffect(() => {Aos.init()},[])
+
+  require("../../assets/images/worksamples/1 (2)c_compressed.jpg")
+
   return (
     <StyledContainer>
       <StyledSampleContainer>
         <StyledWorkImage src={image1} />
         <StyledWorkImage src={image2} />
+        <StyledWorkImage src={image3} />
+        <StyledWorkImage src={image4} />
+        <StyledWorkImage src={image5} />
+        <StyledWorkImage src={image6} />
         <StyledWorkImage src={image7} />
         <StyledWorkImage src={image8} />
       </StyledSampleContainer>
