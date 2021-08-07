6a8b46b62865d6d16d78b14cf6c8f8955a176432
Sarthak Gupta <sarthak.ag2001@gmail.com>
2021-08-07 20:12:21 +0530

    added counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,21 +1,40 @@
 import React from "react";
+import CountUp from "react-countup";
+import ReactVisibilitySensor from "react-visibility-sensor";
 import styled from "styled-components";
 
 const Counter = () => {
-
-return (
+  return (
     <StyledWrapper>
       <StyledCard>
         <h1>Clients</h1>
-        <p>1,175</p>
+        <CountUp start={0} end={1175} duration={0.5}>
+          {({ countUpRef, start }) => (
+            <ReactVisibilitySensor onChange={start}>
+              <p ref={countUpRef}>1,175</p>
+            </ReactVisibilitySensor>
+          )}
+        </CountUp>
       </StyledCard>
       <StyledCard>
         <h1>Countries</h1>
-        <p>98</p>
+        <CountUp start={0} end={98} duration={0.5}>
+          {({ countUpRef, start }) => (
+            <ReactVisibilitySensor onChange={start}>
+              <p ref={countUpRef}>1,175</p>
+            </ReactVisibilitySensor>
+          )}
+        </CountUp>
       </StyledCard>
       <StyledCard>
         <h1>Hours Worked</h1>
-        <p>44,000</p>
+        <CountUp start={0} end={45634} duration={0.5}>
+          {({ countUpRef, start }) => (
+            <ReactVisibilitySensor onChange={start}>
+              <p ref={countUpRef}>1,175</p>
+            </ReactVisibilitySensor>
+          )}
+        </CountUp>
       </StyledCard>
     </StyledWrapper>
   );
@@ -27,39 +46,38 @@ const StyledCard = styled.div`
   text-align: center;
   background-color: white;
   margin: 2rem;
-  
+
   padding: 2rem;
   box-shadow: 2px 2px 2px rgba(255, 255, 255, 0.2);
   h1 {
-      margin-bottom: 2rem;
-      font-size: 2.5rem;
-      font-weight: bold;
-      color: black;
+    margin-bottom: 2rem;
+    font-size: 2.5rem;
+    font-weight: bold;
+    color: black;
   }
-  p{
-      margin: 1rem;
-      font-weight: 200;
-      font-size: 5rem;
-      color: black;
-
+  p {
+    margin: 1rem;
+    font-weight: 200;
+    font-size: 5rem;
+    color: black;
   }
 
   @media only screen and (max-width: 768px) {
-    h1{
+    h1 {
       font-size: 1.5rem;
     }
-    p{
+    p {
       font-size: 3rem;
     }
   }
 `;
 
 const StyledWrapper = styled.div`
-box-sizing: border-box;
-max-width: 100%;
-justify-content: center;
-align-items: center;
-flex-wrap: wrap;
+  box-sizing: border-box;
+  max-width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
   padding: 2rem;
   width: fit-content;
   display: flex;
